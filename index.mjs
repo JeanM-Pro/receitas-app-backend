a50dd8ebf74e326c12b05a92623317bc79c29854
Jean Centeno <jeancenteno54@gmail.com>
2023-09-09 12:49:18 -0300

    se configuro el CORS

--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://receitas-app-backend-production.up.railway.app",
+  "https://receitas-toti.web.app",
 ];
 
 app.use(express.json());
